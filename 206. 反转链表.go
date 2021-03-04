b1e704246eb76e6ab4804327e7867dba331c64a0
Karlven <karlven@outlook.com>
2021-03-04 16:58:50 +0800

    Initial commit

---
+++ 206. 反转链表.go
@@ -0,0 +1,21 @@
+package main
+
+type ListNode struct {
+	Val int
+	Next *ListNode
+}
+
+func reverseList(head *ListNode) *ListNode {
+	cur := head
+	var prev *ListNode
+	for cur != nil {
+		temp := cur.Next
+		cur.Next = prev;
+		prev = cur
+		cur = temp
+	}
+	return prev
+}
+func main() {
+	
+}
