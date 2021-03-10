f25ac87a643ff608efd5b347b6352cde1ea1c1d4
Karlven <karlven@outlook.com>
2021-03-10 09:03:20 +0800

    calculator only support + and -

---
+++ 字符串/224. 基本计算器.go
@@ -0,0 +1,39 @@
+package main
+
+import "fmt"
+
+func calculate(s string) int {
+	opt := []int{1}
+	sign := 1
+	ans := 0
+	for i := 0; i < len(s); {
+		switch s[i] {
+		case ' ':
+			i++
+		case '+':
+			sign = opt[len(opt)-1]
+			i++
+		case '-':
+			sign = -opt[len(opt)-1]
+			i++
+		case '(':
+			opt = append(opt, sign)
+			i++
+		case ')':
+			opt = opt[:len(opt)-1]
+			i++
+		default:
+			num := 0
+			for ; i < len(s) && s[i] >= '0' && s[i] <= '9'; i++ {
+				num = num*10 + (int)(s[i]-'0')
+			}
+			ans += sign * num
+		}
+	}
+	return ans
+}
+
+func main() {
+	s := "(1+(4+5+2)-3)+(6+8)"
+	fmt.Println(calculate(s))
+}
