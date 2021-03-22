2750765a8e2a74735a6beb565d51ec1d145cd300
KarlvenK <karlven@outlook.com>
2021-03-22 16:35:47 +0800

    Update 191. 位1的个数.go

--- 位运算/191. 位1的个数.go
+++ 位运算/191. 位1的个数.go
@@ -12,3 +12,13 @@ func hammingWeight(num uint32) int {
 func main() {
 
 }
+/*
+题目：给你一个数num，求它二进制形式下，1的个数
+
+要点：
+num & (num - 1) 等于将最右的1置零
+e.g.
+num    = 	  1xxxxx10000
+num - 1= 	  1xxxxx01111
+num & (num - 1) = 1xxxxx00000
+*/
