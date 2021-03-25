ae0d3d843d33d2b4fc3c8f7177e2efab2f64e7db
Karlven <karlven@outlook.com>
2021-03-25 14:56:18 +0800

    new

---
+++ 链表/82. 删除排序链表中的重复元素 II.go
@@ -0,0 +1,65 @@
+package main
+
+import (
+	"fmt"
+)
+
+type ListNode struct {
+	Val  int
+	Next *ListNode
+}
+
+func main() {
+	var n int
+	_, _ = fmt.Scanln(&n)
+	head := &ListNode{
+		-1,
+		nil,
+	}
+	pre := head
+	for i := 0; i < n; i++ {
+		var temp int
+		_, _ = fmt.Scanln(&temp)
+		cur := &ListNode{
+			temp,
+			nil,
+		}
+		pre.Next = cur
+		pre = cur
+	}
+	auto := deleteDuplicates(head.Next)
+	for auto != nil {
+		fmt.Println(auto.Val)
+		auto = auto.Next
+	}
+}
+
+func deleteDuplicates(head *ListNode) *ListNode {
+	if head == nil {
+		return nil
+	}
+	dummyHead := &ListNode{
+		-100000,
+		nil,
+	}
+	pre := dummyHead
+	cur := head
+
+	for cur != nil && cur.Next != nil {
+		if cur.Val == cur.Next.Val {
+			mark := cur.Val
+			for cur != nil && cur.Val == mark {
+				cur = cur.Next
+			}
+		} else {
+			pre.Next = cur
+			pre = cur
+			cur = cur.Next
+			pre.Next = nil // 切断 ！！！！！
+		}
+	}
+	if cur != nil {
+		pre.Next = cur
+	}
+	return dummyHead.Next
+}
