3bf827a53f57a30ae14df4526b3296743f84ab4e
Karlven <karlven@outlook.com>
2021-03-26 12:43:45 +0800

    better solution

--- 链表/83. 删除排序链表中的重复元素.go
+++ 链表/83. 删除排序链表中的重复元素.go
@@ -1,25 +1,40 @@
 package main
 
 func deleteDuplicates(head *ListNode) *ListNode {
-	dummyHead := &ListNode{
-		-1,
-		nil,
+	/*
+		dummyHead := &ListNode{
+			-1,
+			nil,
+		}
+		pre := dummyHead
+		cur := head
+		mark := -123456
+		for cur != nil {
+			if cur.Val != mark {
+				mark = cur.Val
+				pre.Next = cur
+				pre = cur
+				cur = cur.Next
+				pre.Next = nil
+			} else {
+				cur = cur.Next
+			}
+		}
+		return dummyHead.Next
+	*/
+	// better version
+	if head == nil {
+		return nil
 	}
-	pre := dummyHead
 	cur := head
-	mark := -123456
-	for cur != nil {
-		if cur.Val != mark {
-			mark = cur.Val
-			pre.Next = cur
-			pre = cur
-			cur = cur.Next
-			pre.Next = nil
+	for cur.Next != nil {
+		if cur.Val == cur.Next.Val {
+			cur.Next = cur.Next.Next
 		} else {
 			cur = cur.Next
 		}
 	}
-	return dummyHead.Next
+	return head
 }
 
 func main() {
