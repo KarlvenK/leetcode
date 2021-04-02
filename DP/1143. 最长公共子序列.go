95604fa04d31ae8caa31f09bfc7475233d035027
Karlven <karlven@outlook.com>
2021-04-03 00:41:02 +0800

    new

---
+++ DP/1143. 最长公共子序列.go
@@ -0,0 +1,34 @@
+package main
+
+import "fmt"
+
+func longestCommonSubsequence(text1 string, text2 string) int {
+	m, n := len(text1), len(text2)
+	dp := make([][]int, m+1)
+	for i := range dp {
+		dp[i] = make([]int, n+1)
+	}
+
+	for i, a := range text1 {
+		for j, b := range text2 {
+			if a == b {
+				dp[i+1][j+1] = dp[i][j] + 1
+			} else {
+				dp[i+1][j+1] = max(dp[i][j+1], dp[i+1][j])
+			}
+		}
+	}
+	return dp[m][n]
+}
+
+func max(a, b int) int {
+	if a > b {
+		return a
+	}
+	return b
+}
+
+func main() {
+	s1, s2 := "abcde", "ace"
+	fmt.Println(longestCommonSubsequence(s1, s2))
+}
