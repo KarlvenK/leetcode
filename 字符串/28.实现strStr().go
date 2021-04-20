2b473047bd339804ac8fa4796b65471456cfa1f2
Karlven <karlven@outlook.com>
2021-04-20 23:05:03 +0800

    new

---
+++ 字符串/28.实现strStr().go
@@ -0,0 +1,32 @@
+package main
+
+func strStr(haystack string, needle string) int {
+	n, m := len(haystack), len(needle)
+	if m == 0 {
+		return 0
+	}
+	next := make([]int, m)
+
+	for i, j := 1, 0; i < m; i++ {
+		for j > 0 && needle[i] != needle[j] {
+			j = next[j-1]
+		}
+		if needle[i] == needle[j] {
+			j++
+		}
+		next[i] = j
+	}
+
+	for i, j := 0, 0; i < n; i++ {
+		for j > 0 && haystack[i] != needle[j] {
+			j = next[j-1]
+		}
+		if haystack[i] == needle[j] {
+			j++
+		}
+		if j == m {
+			return i - m + 1
+		}
+	}
+	return -1
+}
