193d3267b1c5f01ebb356362dc19349f8e96e5c9
Karlven <karlven@outlook.com>
2021-07-02 00:55:31 +0800

    /

---
+++ 贪心/1833. 雪糕的最大数量.py
@@ -0,0 +1,9 @@
+class Solution:
+    def maxIceCream(self, costs: List[int], coins: int) -> int:
+        costs.sort()
+        ans = 0
+        for price in costs:
+            if coins >= price:
+                ans += 1
+                coins -= price
+        return ans
